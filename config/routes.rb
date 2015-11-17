75ef46e81203f919f59f3f16fdc05ab44b7d3fc3
toffguns <dugchun@aol.com>
2015-11-17 22:57:25 +0000

    add pagination

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,11 @@ Rails.application.routes.draw do
 #   delete '/recipes/:id', to: 'recipes#destroy'
   
   
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+  
+  
 end
