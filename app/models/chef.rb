aa8aab4a912d57c62c4844a403c729fe44bdad26
toffguns <dugchun@aol.com>
2015-11-19 00:03:51 +0000

    create chefs resources and authentication

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -11,5 +11,5 @@ class Chef < ActiveRecord::Base
                                     format: {with: VALID_EMAIL_REGEX}
                                     
                                     
-  
+  has_secure_password
 end
